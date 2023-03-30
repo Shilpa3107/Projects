d9ad15ed424156fee684c70363b3fa10fdc0ef95
Shilpa <shilpa.sinha3107@gmail.com>
2023-03-30 21:21:48 +0530

    Using try block

--- Calculator.java
+++ Calculator.java
@@ -2,39 +2,44 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        double num1 , num2;
-        char oper;
-        double result = 0.0;
 
-        System.out.print("Enter first number : ");
-        num1 = sc.nextInt();
-        System.out.println();
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            double num1 , num2;
+            char oper;
+            double result = 0.0;
 
-        System.out.print("Enter second number : ");
-        num2 = sc.nextInt();
-        System.out.println();
+            System.out.print("Enter first number : ");
+            num1 = sc.nextInt();
+            System.out.println();
 
-        System.out.println("Make sure that num2 should not be equal to 0 in case of operator '/' as it is not defined.");
-        System.out.print("Enter operator (+,-,*,/) : ");
-        oper = sc.next().charAt(0);
-        System.out.println();
+            System.out.print("Enter second number : ");
+            num2 = sc.nextInt();
+            System.out.println();
 
-        switch(oper){
-            case '+' : result = num1 + num2;
-            break;
+            System.out.println("Make sure that num2 should not be equal to 0 in case of operator '/' as it is not defined.");
+            System.out.print("Enter operator (+,-,*,/) : ");
+            oper = sc.next().charAt(0);
+            System.out.println();
 
-            case '-' : result = num1 - num2;
-            break;
+            
+            switch(oper){
+                case '+' : result = num1 + num2;
+                break;
 
-            case '*' : result = num1 * num2;
-            break;
+                case '-' : result = num1 - num2;
+                break;
 
-            case '/' : result = num1 / num2;
-            break;
+                case '*' : result = num1 * num2;
+                break;
 
-            default : System.out.println("Invalid operator");
+                case '/' : result = num1 / num2;
+                break;
+
+                default : System.out.println("Invalid operator");
+            }
+            System.out.println("Result : "+result);
         }
-        System.out.println("Result : "+result);
     }
+    
 }
