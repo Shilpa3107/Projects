d6dcdfda047e21a95ff9e561c6b4f56b20c67376
Shilpa <shilpa.sinha3107@gmail.com>
2023-04-15 03:34:48 +0530

    Adding serial number

--- com/example/demo/CollegeDAO.java
+++ com/example/demo/CollegeDAO.java
@@ -9,9 +9,9 @@ public class CollegeDAO{
     private static Colleges list = new Colleges();
 
     static {
-        list.getCollegeList().add( new College("IIT Madras", "B.TECH",800000.0, 4, "AC", 20000.0));
-        list.getCollegeList().add( new College("IIT Bombay", "B.SC",80000.0, 3, "AC", 20000.0));
-        list.getCollegeList().add( new College("IIT Delhi", "B.A",80000.0, 4, "AC", 2000.0));
+        list.getCollegeList().add( new College(1,"IIT Madras", "B.TECH",800000.0, 4, "AC", 20000.0));
+        list.getCollegeList().add( new College(2,"IIT Bombay", "B.SC",80000.0, 3, "AC", 20000.0));
+        list.getCollegeList().add( new College(3,"IIT Delhi", "B.A",80000.0, 4, "AC", 2000.0));
 
     }
 
