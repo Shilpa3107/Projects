e350812adf7dc26784798bbeadbad307eed61c9d
Shilpa <shilpa.sinha3107@gmail.com>
2023-04-30 23:01:39 +0530

    Making a pattern

---
+++ pattern.java
@@ -0,0 +1,17 @@
+public class pattern {
+    public static void main(String args[]){
+        int n = 5;
+        int c = 10;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print(c+" ");
+                    c++;
+                
+            }
+           c++;
+            System.out.println();
+        }
+        
+
+    } 
+}
